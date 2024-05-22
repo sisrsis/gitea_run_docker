5f85706df027d4903ac5bcdafcba20021f3a038f
SISRSIS <virussisrsis@gmail.com>
2024-05-22 22:43:59 +0330

    create

---
+++ git_runner.bash
@@ -0,0 +1,4 @@
+source config.env
+sudo docker stop   gitea_runner 
+sudo docker rm     gitea_runner 
+sudo docker run --name gitea_runner -d   --restart unless-stopped   --net host -e GITEA_INSTANCE_URL=$gitea_url  -e GITEA_RUNNER_REGISTRATION_TOKEN=$token_gitea_runner   -v ./data_runner:/data -v /var/run/docker.sock:/var/run/docker.sock  gitea/act_runner:latest 
