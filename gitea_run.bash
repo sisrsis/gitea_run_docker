111f1b8e0c2ab5df4f9161a5c99f05a9ceb37bf3
sisrsis <51427417+sisrsis@users.noreply.github.com>
2024-05-30 16:13:30 +0330

    Update gitea_run.bash

--- gitea_run.bash
+++ gitea_run.bash
@@ -4,4 +4,5 @@ mkdir data
 sudo docker network create  giteas
 sudo docker run --name mariadb       --network giteas -p 3306:3306 -e MARIADB_ROOT_PASSWORD=$MYSQL_root_password   -d  --restart unless-stopped mariadb
 sudo docker run  --name gitea   -v  ./data:/data/ -v ./config:/etc/gitea  -p 3000:3000 -p 2222:2222  -e GITEA__database__DB_TYPE=mysql --net giteas -e GITEA__database__HOST=mariadb:3306 -e GITEA__database__NAME=gitea  -e GITEA__database__USER=root -e  GITEA__database__PASSWD=$MYSQL_root_password -d --restart unless-stopped  gitea/gitea
+sleep 10
 mysql -u root -p --password=$MYSQL_root_password  --port=3306 --protocol=TCP    < user.sql;
