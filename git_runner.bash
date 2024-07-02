d4d3e69be7c5aec420ce17d2f4401dac65f11ce3
sisrsis <51427417+sisrsis@users.noreply.github.com>
2024-07-02 16:32:45 +0330

    Update git_runner.bash

--- git_runner.bash
+++ git_runner.bash
@@ -1,5 +1,4 @@
 source config.env
-mkdir data_runner
 sudo docker stop   gitea_runner 
 sudo docker rm     gitea_runner 
-sudo docker run --name gitea_runner -d   --restart unless-stopped   --net host -e GITEA_INSTANCE_URL=$gitea_url  -e GITEA_RUNNER_REGISTRATION_TOKEN=$token_gitea_runner   -v ./data_runner:/data -v /var/run/docker.sock:/var/run/docker.sock  gitea/act_runner:latest 
+sudo docker run --name gitea_runner -d   --restart unless-stopped  --net host  -e GITEA_RUNNER_LABELS="run_monier" -v ./data_runner:/data -e GITEA_RUNNER_LABELS="run_monier" -e GITEA_RUNNER_NAME="moniner"  -e GITEA_INSTANCE_URL=$gitea_url  -v ./config.yaml:/config.yaml -e CONFIG_FILE=/config.yaml  -e GITEA_RUNNER_REGISTRATION_TOKEN=$token_gitea_runner    -v /var/run/docker.sock:/var/run/docker.sock  gitea/act_runner:latest
